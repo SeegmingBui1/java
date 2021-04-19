20720fc2686e333d42fed2e2316aa33f2d5629bd
SeegmingBui1 <wQ6604782529682@163.com>
2021-04-19 16:11:12 +0800

    演示do..while和break跳转功能

--- 1_base_syntax/stCtl.java
+++ 1_base_syntax/stCtl.java
@@ -2,8 +2,12 @@ public class stCtl {
     static public void main(String [] args) {
         int i, j;
 
+        // 此标签只能放在循环语句的前面
+        Q:
         for (i = 1; i < 10; i++) {
             for (j = 1; j <= i; j++)  {
+                if (j > 5)
+                    break Q; //在退出多次循环时使用
                 if (j == 1) {
                     System.out.printf("\033[31m%d x %d = %d\033[0m ", j, i, i * j);
                 } else {
