1e3b09abe140904e5d168c7996ddadf62b6486ac
SeegmingBui1 <wQ6604782529682@163.com>
2021-04-21 17:07:06 +0800

    完成基本语法的作业

--- 2_function/Homework.java
+++ 2_function/Homework.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Homework {
 
     static Scanner sc;
+    static String numString = "0123456789ABCDEF";
 
     static public int isYear(int year) {
         return (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) ? 1 : 0;
@@ -240,6 +241,144 @@ public class Homework {
         }
     }
 
+    // 3. 用一个循环输出等腰梯型(可指定顶和高)
+    static public void trapezoid() {
+        System.out.printf("请输入梯形的顶: ");
+        int t = sc.nextInt();
+        System.out.printf("请输入梯形的高: ");
+        int h = sc.nextInt();
+        int i, j;
+
+        /*
+         * 3 5
+         * ####***####
+         *    *****
+         *   *******
+         *  *********
+         * ***********
+         */
+
+        /*
+         * for (i = 0; i < h; i++) {
+         *     for (j = 0; j < h - i - 1; j++) {
+         *         System.out.printf("   ");
+         *     }
+         *     for (j = 0; j < t + i * 2; j++) {
+         *         System.out.printf(" * ");
+         *     }
+         *     System.out.println();
+         * }
+         */
+        int w = (h - 1) * 2 + t;
+        int m = w * h;
+        int r, c, s;
+
+        for (i = 0; i < m; i++) {
+            c = i % w;
+            r = i / w;
+
+            // if (c >= h - r - 1 && c < h - r - 1 + t + r * 2) {
+            s = h - r - 1;
+            if (c >= s && c < w - s) {
+                System.out.printf(" * ");
+            } else {
+                System.out.printf("   ");
+            }
+
+            if (c == w - 1) {
+                System.out.println();
+            }
+        }
+    }
+
+    // 7. 4, 5, 6, 7能组合多少个不能被4整除的4位数(位数不能相同, 思考: 一个循环实现)
+    static public void genNum() {
+        int i, j, k, l, s, sum = 0, count = 0;
+
+        /*
+         * for (i = 4; i < 8; ++i) {
+         *     for (j = 4; j < 8; ++j) {
+         *         for (k = 4; k < 8; ++k) {
+         *             for (l = 4; l < 8; ++l) {
+         *                 if (i == j || i == k || i == l
+         *                            || j == k || j == l || k == l) {
+         *                     continue;
+         *                 }
+         *                 s = i * 1000 + j * 100 + k * 10 + l;
+         *                 if (s % 4 != 0) {
+         *                     System.out.printf("%d ", s);
+         *                     count++;
+         *                 }
+         *             }
+         *         }
+         *     }
+         * }
+         */
+
+        sum = 4 + 5 + 6 + 7;
+        for (i = 4; i < 8; ++i) {
+            for (j = 4; j < 8; ++j) {
+                if (i == j)
+                    continue;
+                for (k = 4; k < 8; ++k) {
+                    if (i == k || j == k) {
+                        continue;
+                    }
+                    l = sum - i - j - k;
+                    s = i * 1000 + j * 100 + k * 10 + l;
+                    if (s % 4 != 0) {
+                        System.out.printf("%d ", s);
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.printf("\ncount = %d\n", count);
+
+        for (i = 4567, count = 0; i <= 7654; ++i) {
+            j = i / 1000 % 10;
+            k = i / 100 % 10;
+            l = i / 10 % 10;
+            s = i % 10;
+            if (j >= 4 && j <= 7 && k >= 4 && k <= 7
+                    && l >= 4 && l <= 7 && s >= 4 && s <= 7
+                    && j != k && j != l && j != s && k != l && k != s
+                    && l != s && i % 4 != 0) {
+                    System.out.printf("%d ", i);
+                    count++;
+            }
+        }
+        System.out.printf("\ncount = %d\n", count);
+    }
+
+    // 12. 输出十进制转八进制的数(实现printf("%o")的功能)
+    static public void decTo(int num, int bit) {
+        if (num == 0)
+            return;
+        decTo(num / bit, bit);
+        System.out.printf("%c", numString.charAt(num % bit));
+    }
+
+    static public void decToInput() {
+        System.out.printf("请输入数字: ");
+        int num = sc.nextInt();
+        System.out.printf("请输入转化的进制: ");
+        int bit = sc.nextInt();
+        decTo(num, bit);
+        System.out.println();
+    }
+
+    // 13. 猴子吃桃，每天吃前一天的一半加1个桃子，第5天吃完剩一个，求还没吃之前的总数
+    static public int eat(int day) {
+        if (day == 0)
+            return 1;
+        return (eat(day - 1) + 1) * 2;
+    }
+
+    static public void eatInput() {
+        System.out.printf("Total = %d\n", eat(5));
+    }
+
     static public void main(String [] args) {
         sc = new Scanner(System.in);
         // blackFri();
@@ -247,6 +386,10 @@ public class Homework {
         prefectNum();
         flowerNum();
         // palindrome();
-        charTo();
+        // charTo();
+        // trapezoid();
+        // genNum();
+        // decToInput();
+        eatInput();
     }
 }
